58c38ba9f1b4b8cbcba1423cb419f85cd6d780c9
prvillanuevajr <prvillanuevajr@gmail.com>
2023-01-13 06:00:44 +0800

    Apply Hibernate to login validation

--- src/main/java/com/presmelito/mytodo/dao/LoginDao.java
+++ src/main/java/com/presmelito/mytodo/dao/LoginDao.java
@@ -2,8 +2,13 @@ package com.presmelito.mytodo.dao;
 
 import com.presmelito.mytodo.model.Login;
 import com.presmelito.mytodo.model.User;
+import com.presmelito.mytodo.utils.HibConfiguration;
 import com.presmelito.mytodo.utils.JDBCUtil;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.hibernate.Session;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,27 +18,16 @@ import java.sql.SQLException;
 public class LoginDao {
 
     public boolean validate(Login login) {
-        try (
-                Connection connection = JDBCUtil.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from users where userName = ? and password = ?");
-        ) {
-            preparedStatement.setString(1, login.getUserName());
-            preparedStatement.setString(2, DigestUtils.sha256Hex(login.getPassword()));
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                User user = new User(
-                        resultSet.getLong("id"),
-                        resultSet.getString("firstName"),
-                        resultSet.getString("lastName"),
-                        resultSet.getString("userName"),
-                        resultSet.getString("password")
-                );
-                login.setUser(user);
-                return true;
-            }
-            return false;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try(Session session = HibConfiguration.getSessionFactory().openSession()){
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+            criteriaQuery = criteriaQuery.where(criteriaBuilder.equal(root.get("userName"),login.getUserName()))
+                    .where(criteriaBuilder.equal(root.get("password"),DigestUtils.sha256Hex(login.getPassword())));
+            User user = session.createQuery(criteriaQuery).getSingleResult();
+            login.setUser(user);
+            session.close();
+            return user != null;
         }
     }
 }
